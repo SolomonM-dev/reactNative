cd09125d9d0d7a0d365f673d22513b9a015a7ae9
Solomon M <solomon.m@mblhightech.com>
2023-12-08 12:11:58 +0200

    fire-base init

---
+++ firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAPBEDr7R8wC8mQxK_YP-muMfXX8ORYz8Y",
+  authDomain: "fitnessapp-7fbfd.firebaseapp.com",
+  projectId: "fitnessapp-7fbfd",
+  storageBucket: "fitnessapp-7fbfd.appspot.com",
+  messagingSenderId: "734683270344",
+  appId: "1:734683270344:web:a77e20d4d006c75dce29fd",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export { app, auth };
